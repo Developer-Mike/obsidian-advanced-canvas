0b816ba9ac1172d513b37b8f311dd60d7862efe7
Developer-Mike <delta.cephei@outlook.com>
2024-01-29 17:30:21 +0100

    Added more flowchart shapes

--- src/canvas-extensions/flowchart-canvas-extension.ts
+++ src/canvas-extensions/flowchart-canvas-extension.ts
@@ -21,7 +21,7 @@ const CUSTOM_NODES: CustomNode[] = [
     }
   },
   {
-    alias: ['process', 'rectangle', 'rect'],
+    alias: ['process', 'rectangle', 'rect', 'center'],
     className: 'rectangle-node',
     defaultSize: { width: 150, height: 60 },
     menuName: 'Rectangle shaped card',
@@ -42,6 +42,56 @@ const CUSTOM_NODES: CustomNode[] = [
     icon: () => {
       addIcon('parallelogram', '<rect transform="skewX(-20)" rx="5" height="50" width="70" y="25" x="35" stroke-width="5.5" stroke="currentColor" fill="transparent"/>')
       return 'parallelogram'
+    }
+  },
+  {
+    alias: ['reference', 'ref', 'circle'],
+    className: 'circle-node',
+    defaultSize: { width: 50, height: 50 },
+    menuName: 'Circle shaped card',
+    icon: () => 'circle'
+  },
+  {
+    alias: ['predefined', 'predefined-process'],
+    className: 'predefined-node',
+    defaultSize: { width: 150, height: 60 },
+    menuName: 'Predefined process shaped card',
+    icon: () => {
+      addIcon('predefined-process', `
+        <g stroke-width="1.25" stroke="currentColor" transform="matrix(4.166667,0,0,4.166667,0,0)">
+          <path d="M 4.999687 3 L 19.000312 3 C 20.104688 3 21 3.895312 21 4.999687 L 21 19.000312 C 21 20.104688 20.104688 21 19.000312 21 L 4.999687 21 C 3.895312 21 3 20.104688 3 19.000312 L 3 4.999687 C 3 3.895312 3.895312 3 4.999687 3 Z M 4.999687 3 "/>
+          <path d="M 7 3 L 7 21 "/>
+          <path d="M 17 3 L 17 21 "/>
+        </g>
+      `)
+      return 'predefined-process'
+    }
+  },
+  {
+    alias: ['document', 'doc', 'file'],
+    className: 'document-node',
+    defaultSize: { width: 150, height: 60 },
+    menuName: 'Document shaped card',
+    icon: () => {
+      addIcon('document', `
+        <path transform="translate(0, 5)" stroke="currentColor" stroke-width="5" d="M83.75 25C85.82 25 87.5 26.68 87.5 28.75L87.5 64.375Q68.75 54.25 50 64.375 31.25 74.5 12.5 64.375L12.5 30.625 12.5 28.75C12.5 26.68 14.18 25 16.25 25Z"/>
+      `)
+      return 'document'
+    }
+  },
+  {
+    alias: ['database', 'data'],
+    className: 'database-node',
+    defaultSize: { width: 150, height: 60 },
+    menuName: 'Database shaped card',
+    icon: () => {
+      addIcon('database-node', `
+        <g transform="translate(20, 20)" stroke-width="5" stroke="currentColor">
+          <path d="M 1 51 L 1 11 C 1 5.48 14.43 1 31 1 C 47.57 1 61 5.48 61 11 L 61 51 C 61 56.52 47.57 61 31 61 C 14.43 61 1 56.52 1 51 Z"/>
+          <path d="M 1 11 C 1 16.52 14.43 21 31 21 C 47.57 21 61 16.52 61 11"/>
+        </g>
+      `)
+      return 'database-node'
     }
   }
 ]
