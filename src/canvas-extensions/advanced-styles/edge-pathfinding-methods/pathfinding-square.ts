ee5023b2648491677969749004cbd8998124130f
Developer-Mike <delta.cephei@outlook.com>
2024-07-03 17:36:37 +0200

    Fixed wrong label postion for square pathfinding method

--- src/canvas-extensions/advanced-styles/edge-pathfinding-methods/pathfinding-square.ts
+++ src/canvas-extensions/advanced-styles/edge-pathfinding-methods/pathfinding-square.ts
@@ -8,6 +8,7 @@ import CanvasHelper from "src/utils/canvas-helper"
 export default class EdgePathfindingSquare extends EdgePathfindingMethod {
   getPath(_plugin: AdvancedCanvasPlugin, _canvas: Canvas, fromPos: Position, fromBBoxSidePos: Position, fromSide: Side, toPos: Position, toBBoxSidePos: Position, toSide: Side, _isDragging: boolean): EdgePath {
     let pathArray: Position[] = []
+    let center: Position = { x: 0, y: 0 }
 
     if (fromSide === toSide) {
       // Same side -> Make a U
@@ -28,6 +29,11 @@ export default class EdgePathfindingSquare extends EdgePathfindingMethod {
           toPos
         ]
       }
+
+      center = { 
+        x: (pathArray[1].x + pathArray[2].x) / 2,
+        y: (pathArray[1].y + pathArray[2].y) / 2
+      }
     } else if (BBoxHelper.isHorizontal(fromSide) === BBoxHelper.isHorizontal(toSide)) {
       // Same axis, different side -> Make a Z
       if (BBoxHelper.isHorizontal(fromSide)) {
@@ -45,6 +51,11 @@ export default class EdgePathfindingSquare extends EdgePathfindingMethod {
           toPos
         ]
       }
+      
+      center = { 
+        x: (fromBBoxSidePos.x + toBBoxSidePos.x) / 2, 
+        y: (fromBBoxSidePos.y + toBBoxSidePos.y) / 2 
+      }
     } else {
       // Different axis -> Make a L
       if (BBoxHelper.isHorizontal(fromSide)) {
@@ -60,14 +71,16 @@ export default class EdgePathfindingSquare extends EdgePathfindingMethod {
           toPos
         ]
       }
+
+      center = { 
+        x: pathArray[1].x,
+        y: pathArray[1].y
+      }
     }
 
     return {
       svgPath: SvgPathHelper.pathArrayToSvgPath(pathArray, false),
-      center: { 
-        x: (fromBBoxSidePos.x + toBBoxSidePos.x) / 2, 
-        y: (fromBBoxSidePos.y + toBBoxSidePos.y) / 2 
-      },
+      center: center,
       rotateArrows: false
     }
   }
