a61b696a95b4e2a4771ef8ba49df442987a8a6d7
Developer-Mike <delta.cephei@outlook.com>
2024-07-05 08:45:39 +0200

    Fixed interaction exposer

--- src/canvas-extensions/dataset-exposers/node-interaction-exposer.ts
+++ src/canvas-extensions/dataset-exposers/node-interaction-exposer.ts
@@ -12,16 +12,23 @@ export default class NodeInteractionExposerExtension extends CanvasExtension {
     this.plugin.registerEvent(this.plugin.app.workspace.on(
       CanvasEvent.NodeInteraction,
       (canvas: Canvas, node: CanvasNode) => {
+        const nodeData = node?.getData()
+        if (!nodeData) return
+        
         const interactionEl = canvas.nodeInteractionLayer.interactionEl
         if (!interactionEl) return
-    
-        for (const dataKey of getExposedNodeData(this.plugin.settings)) {
-          const datasetKey = TARGET_NODE_DATASET_PREFIX + dataKey.toString().charAt(0).toUpperCase() + dataKey.toString().slice(1)
-    
-          const dataValue = node?.getData()[dataKey]
 
-          if (!dataValue) delete interactionEl.dataset[datasetKey]
-          else interactionEl.dataset[datasetKey] = dataValue
+        for (const exposedDataKey of getExposedNodeData(this.plugin.settings)) {
+          const datasetPairs = nodeData[exposedDataKey] instanceof Object
+            ? Object.entries(nodeData[exposedDataKey])
+            : [[exposedDataKey, nodeData[exposedDataKey]]]
+
+          for (const [key, value] of datasetPairs) {
+            const modifiedKey = TARGET_NODE_DATASET_PREFIX + key.toString().charAt(0).toUpperCase() + key.toString().slice(1)
+            
+            if (!value) delete interactionEl.dataset[modifiedKey]
+            else interactionEl.dataset[modifiedKey] = value
+          }
         }
       }
     ))
