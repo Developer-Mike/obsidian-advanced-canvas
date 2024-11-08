8d8a0825af346cb7730a19721374317254b0ff01
Developer-Mike <delta.cephei@outlook.com>
2024-11-08 14:59:09 +0100

    Added inbuilt canvas events to object

--- src/canvas-extensions/encapsulate-canvas-extension.ts
+++ src/canvas-extensions/encapsulate-canvas-extension.ts
@@ -3,6 +3,7 @@ import { Canvas } from "src/@types/Canvas"
 import { FileNameModal } from "src/utils/modal-helper"
 import CanvasHelper from "src/utils/canvas-helper"
 import CanvasExtension from "../core/canvas-extension"
+import { CanvasEvent } from "src/core/events"
 
 const ENCAPSULATED_FILE_NODE_SIZE = { width: 300, height: 300 }
 
@@ -23,7 +24,7 @@ export default class EncapsulateCanvasExtension extends CanvasExtension {
 
     /* Add encapsulate option to context menu */
     this.plugin.registerEvent(this.plugin.app.workspace.on(
-      'canvas:selection-menu',
+      CanvasEvent.SelectionContextMenu,
       (menu: Menu, canvas: Canvas) => {
         menu.addItem((item) =>
           item
