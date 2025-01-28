22b254449c859d1fdae00ff6df44e4c72232f40b
Developer-Mike <delta.cephei@outlook.com>
2025-01-28 16:31:53 +0100

    Successfully added backlinks and outgoing links patchers

--- src/patchers/backlinks-patcher.ts
+++ src/patchers/backlinks-patcher.ts
@@ -1,17 +1,57 @@
 import PatchHelper from "src/utils/patch-helper"
 import Patcher from "./patcher"
+import { TAbstractFile, TFile, TFolder } from "obsidian"
 
 export default class BacklinksPatcher extends Patcher {
+  private isRecomputingBacklinks: boolean = false
+
   protected async patch() {
     if (!this.plugin.settings.getSetting('canvasLinksFeatureEnabled')) return
 
     const that = this
-    PatchHelper.tryPatchWorkspacePrototype(this.plugin, () => {
-
-    }, {
-      foo: (next: any) => function (...args: any[]) {
-        return next.call(this, ...args)
+    const backlinkPatch = PatchHelper.tryPatchWorkspacePrototype(this.plugin, () => (
+      (this.plugin.app.workspace.getLeavesOfType('backlink').first()?.view as any)?.backlink
+    ), {
+      recomputeBacklink: (next: any) => function (file: TFile, ...args: any[]) {
+        that.isRecomputingBacklinks = true
+        const result = next.call(this, file, ...args)
+        that.isRecomputingBacklinks = false
+        return result
       }
     })
+
+    const vaultPatch = PatchHelper.patchObjectPrototype(this.plugin, this.plugin.app.vault, {
+      recurseChildrenAC: (_next: any) => function (origin: TAbstractFile, traverse: (file: TAbstractFile) => void) {
+        for (var stack = [origin]; stack.length > 0;) {
+          var current = stack.pop()
+          if (current) {
+            traverse(current)
+
+            // If the current item is a folder, add its children to the stack
+            if (current instanceof TFolder) stack = stack.concat(current.children)
+          }
+        }
+      },
+      getMarkdownFiles: (next: any) => function (file: TFile, ...args: any[]) {
+        if (!that.isRecomputingBacklinks) return next.call(this, file, ...args)
+
+        // If we are recomputing backlinks, we need to include markdown as well as canvas files
+        var files: TFile[] = []
+        var root = this.getRoot()
+
+        this.recurseChildrenAC(root, (child: TAbstractFile) => {
+          if (child instanceof TFile && (child.extension === "md" || child.extension === "canvas")) {
+            files.push(child)
+          }
+        })
+
+        return files
+      }
+    })
+
+    const [backlink] = await Promise.all([backlinkPatch, vaultPatch])
+
+    // Patch successful - recompute backlinks
+    backlink.recomputeBacklink(this.plugin.app.workspace.getActiveFile())
   }
 }
--- src/patchers/outgoing-links-patcher.ts
+++ src/patchers/outgoing-links-patcher.ts
@@ -6,12 +6,41 @@ export default class OutgoingLinksPatcher extends Patcher {
     if (!this.plugin.settings.getSetting('canvasLinksFeatureEnabled')) return
 
     const that = this
-    PatchHelper.tryPatchWorkspacePrototype(this.plugin, () => {
+    const outgoingLinkPatch = PatchHelper.tryPatchWorkspacePrototype(this.plugin, () => (
+      (this.plugin.app.workspace.getLeavesOfType('outgoing-link').first()?.view as any)?.outgoingLink
+    ), {
+      recomputeLinks: (next: any) => function (...args: any[]) {
+        const isCanvas = this.file?.extension === 'canvas'
 
-    }, {
-      foo: (next: any) => function (...args: any[]) {
-        return next.call(this, ...args)
+        // Trick the app into thinking that the file is a markdown file
+        if (isCanvas) this.file.extension = 'md'
+
+        const result = next.call(this, ...args)
+
+        // Revert the extension change
+        if (isCanvas) this.file.extension = 'canvas'
+
+        return result
+      },
+      recomputeUnlinked: (next: any) => function (...args: any[]) {
+        const isCanvas = this.file?.extension === 'canvas'
+
+        // Trick the app into thinking that the file is a markdown file
+        if (isCanvas) this.file.extension = 'md'
+
+        const result = next.call(this, ...args)
+
+        // Revert the extension change
+        if (isCanvas) this.file.extension = 'canvas'
+
+        return result
       }
     })
+
+    const [outgoingLink] = await Promise.all([outgoingLinkPatch])
+
+    // Patch successful - recompute outgoing links
+    outgoingLink.recomputeLinks()
+    outgoingLink.recomputeUnlinked()
   }
 }
