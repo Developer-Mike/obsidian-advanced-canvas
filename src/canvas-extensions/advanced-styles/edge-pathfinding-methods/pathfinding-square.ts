1df2657b279955e75d0d5027e52a3e9e8bf48892
Developer-Mike <delta.cephei@outlook.com>
2025-03-07 18:51:44 +0100

    Improved square pathfinding code

--- src/canvas-extensions/advanced-styles/edge-pathfinding-methods/pathfinding-square.ts
+++ src/canvas-extensions/advanced-styles/edge-pathfinding-methods/pathfinding-square.ts
@@ -9,58 +9,63 @@ const ROUNDED_EDGE_RADIUS = 5
 
 export default class EdgePathfindingSquare extends EdgePathfindingMethod {
   getPath(plugin: AdvancedCanvasPlugin, _canvas: Canvas, fromPos: Position, fromBBoxSidePos: Position, fromSide: Side, toPos: Position, toBBoxSidePos: Position, toSide: Side): EdgePath {
-    let pathArray: Position[] = []
-    let center: Position = { x: 0, y: 0 }
-
+    let pathArray: Position[]
+    let center: Position
+    const isFromHorizontal = BBoxHelper.isHorizontal(fromSide)
+    const isToHorizontal = BBoxHelper.isHorizontal(toSide)
+  
     if (fromSide === toSide) {
-      // Same side -> Make a U
       const direction = BBoxHelper.direction(fromSide)
 
-      if (BBoxHelper.isHorizontal(fromSide)) {
+      if (isFromHorizontal) {
+        const x = Math.max(fromBBoxSidePos.x, toBBoxSidePos.x) + direction * CanvasHelper.GRID_SIZE
         pathArray = [
           fromPos,
-          { x: Math.max(fromBBoxSidePos.x, toBBoxSidePos.x) + direction * CanvasHelper.GRID_SIZE, y: fromBBoxSidePos.y },
-          { x: Math.max(fromBBoxSidePos.x, toBBoxSidePos.x) + direction * CanvasHelper.GRID_SIZE, y: toBBoxSidePos.y },
+          { x: x, y: fromBBoxSidePos.y },
+          { x: x, y: toBBoxSidePos.y },
           toPos
         ]
       } else {
+        const y = Math.max(fromBBoxSidePos.y, toBBoxSidePos.y) + direction * CanvasHelper.GRID_SIZE
         pathArray = [
           fromPos,
-          { x: fromBBoxSidePos.x, y: Math.max(fromBBoxSidePos.y, toBBoxSidePos.y) + direction * CanvasHelper.GRID_SIZE },
-          { x: toBBoxSidePos.x, y: Math.max(fromBBoxSidePos.y, toBBoxSidePos.y) + direction * CanvasHelper.GRID_SIZE },
-          toPos
-        ]
-      }
-
-      center = { 
-        x: (pathArray[1].x + pathArray[2].x) / 2,
-        y: (pathArray[1].y + pathArray[2].y) / 2
-      }
-    } else if (BBoxHelper.isHorizontal(fromSide) === BBoxHelper.isHorizontal(toSide)) {
-      // Same axis, different side -> Make a Z
-      if (BBoxHelper.isHorizontal(fromSide)) {
-        pathArray = [
-          fromPos,
-          { x: fromBBoxSidePos.x + (toBBoxSidePos.x - fromBBoxSidePos.x) / 2, y: fromBBoxSidePos.y },
-          { x: fromBBoxSidePos.x + (toBBoxSidePos.x - fromBBoxSidePos.x) / 2, y: toBBoxSidePos.y },
-          toPos
-        ]
-      } else {
-        pathArray = [
-          fromPos,
-          { x: fromBBoxSidePos.x, y: fromBBoxSidePos.y + (toBBoxSidePos.y - fromBBoxSidePos.y) / 2 },
-          { x: toBBoxSidePos.x, y: fromBBoxSidePos.y + (toBBoxSidePos.y - fromBBoxSidePos.y) / 2 },
+          { x: fromBBoxSidePos.x, y: y },
+          { x: toBBoxSidePos.x, y: y },
           toPos
         ]
       }
       
-      center = { 
-        x: (fromBBoxSidePos.x + toBBoxSidePos.x) / 2, 
-        y: (fromBBoxSidePos.y + toBBoxSidePos.y) / 2 
+      center = {
+        x: (pathArray[1].x + pathArray[2].x) / 2,
+        y: (pathArray[1].y + pathArray[2].y) / 2
+      }
+    } else if (isFromHorizontal === isToHorizontal) {
+      // Z shape: same axis, different sides
+      if (isFromHorizontal) {
+        const midX = fromBBoxSidePos.x + (toBBoxSidePos.x - fromBBoxSidePos.x) / 2
+        pathArray = [
+          fromPos,
+          { x: midX, y: fromBBoxSidePos.y },
+          { x: midX, y: toBBoxSidePos.y },
+          toPos
+        ]
+      } else {
+        const midY = fromBBoxSidePos.y + (toBBoxSidePos.y - fromBBoxSidePos.y) / 2
+        pathArray = [
+          fromPos,
+          { x: fromBBoxSidePos.x, y: midY },
+          { x: toBBoxSidePos.x, y: midY },
+          toPos
+        ]
+      }
+
+      center = {
+        x: (fromBBoxSidePos.x + toBBoxSidePos.x) / 2,
+        y: (fromBBoxSidePos.y + toBBoxSidePos.y) / 2
       }
     } else {
-      // Different axis -> Make a L
-      if (BBoxHelper.isHorizontal(fromSide)) {
+      // L shape: different axis
+      if (isFromHorizontal) {
         pathArray = [
           fromPos,
           { x: toBBoxSidePos.x, y: fromBBoxSidePos.y },
@@ -74,18 +79,13 @@ export default class EdgePathfindingSquare extends EdgePathfindingMethod {
         ]
       }
 
-      center = { 
-        x: pathArray[1].x,
-        y: pathArray[1].y
-      }
+      center = { x: pathArray[1].x, y: pathArray[1].y }
     }
-
-    return {
-      svgPath: plugin.settings.getSetting('edgeStyleSquarePathRounded') ?
-        SvgPathHelper.pathArrayToRoundedSvgPath(pathArray, ROUNDED_EDGE_RADIUS) :
-        SvgPathHelper.pathArrayToSvgPath(pathArray),
-      center: center,
-      rotateArrows: false
-    }
+  
+    const svgPath = plugin.settings.getSetting('edgeStyleSquarePathRounded')
+      ? SvgPathHelper.pathArrayToRoundedSvgPath(pathArray, ROUNDED_EDGE_RADIUS)
+      : SvgPathHelper.pathArrayToSvgPath(pathArray)
+  
+    return { svgPath, center, rotateArrows: false }
   }
 }
