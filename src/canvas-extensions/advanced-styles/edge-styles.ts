b03e137344ca635193d479818118aeb4a8cfb897
Epeon <60582251+EpeonGamer@users.noreply.github.com>
2025-11-04 23:03:35 +0200

    Update edge-styles.ts

--- src/canvas-extensions/advanced-styles/edge-styles.ts
+++ src/canvas-extensions/advanced-styles/edge-styles.ts
@@ -192,6 +192,8 @@ export default class EdgeStylesExtension extends CanvasExtension {
       return `0 0, 4.95 1.8, 7.5 6.45, 6.6 11.7, 2.7 15, -2.7 15, -6.6 11.7, -7.5 6.45, -4.95 1.8`
     else if (arrowStyle === 'blunt')
       return `-10,8 10,8 10,6 -10,6`
+    else if (arrowStyle === "pin)
+      return `0,30 6,30 3,27 3,17 9,11 2,10 0,-2 -2,10 -9,11 -3,17 -3,27 -6,30`
     else // Default triangle
       return `0,0 6.5,10.4 -6.5,10.4`
   }
